582a1cca2b988fcc4ff18ab05f5eb21a8d569b71
mrciolino <mrciolino@comcast.net>
2024-01-16 16:26:32 +0000

    adding phoneme option linux vs windows

--- src/styletts2/utils.py
+++ src/styletts2/utils.py
@@ -7,6 +7,11 @@ import torchaudio
 import librosa
 import matplotlib.pyplot as plt
 from munch import Munch
+import subprocess
+import sys
+import phonemizer
+
+from .phoneme import PhonemeConverterFactory
 
 
 def get_data_path_list(train_path=None, val_path=None):
@@ -15,17 +20,19 @@ def get_data_path_list(train_path=None, val_path=None):
     if val_path is None:
         val_path = "Data/val_list.txt"
 
-    with open(train_path, 'r', encoding='utf-8', errors='ignore') as f:
+    with open(train_path, "r", encoding="utf-8", errors="ignore") as f:
         train_list = f.readlines()
-    with open(val_path, 'r', encoding='utf-8', errors='ignore') as f:
+    with open(val_path, "r", encoding="utf-8", errors="ignore") as f:
         val_list = f.readlines()
 
     return train_list, val_list
 
+
 def length_to_mask(lengths):
     mask = torch.arange(lengths.max()).unsqueeze(0).expand(lengths.shape[0], -1).type_as(lengths)
-    mask = torch.gt(mask+1, lengths.unsqueeze(1))
+    mask = torch.gt(mask + 1, lengths.unsqueeze(1))
     return mask
+
 
 # for norm consistency loss
 def log_norm(x, mean=-4, std=4, dim=2):
@@ -35,13 +42,15 @@ def log_norm(x, mean=-4, std=4, dim=2):
     x = torch.log(torch.exp(x * std + mean).norm(dim=dim))
     return x
 
+
 def get_image(arrs):
-    plt.switch_backend('agg')
+    plt.switch_backend("agg")
     fig = plt.figure()
     ax = plt.gca()
     ax.imshow(arrs)
 
     return fig
+
 
 def recursive_munch(d):
     if isinstance(d, dict):
@@ -50,8 +59,25 @@ def recursive_munch(d):
         return [recursive_munch(v) for v in d]
     else:
         return d
-    
+
+
 def log_print(message, logger):
     logger.info(message)
     print(message)
-    
+
+
+def phoneme_check(defined_converter):
+    try:
+        if defined_converter == "gruut":
+            phoneme_converter = PhonemeConverterFactory.load_phoneme_converter(defined_converter)
+        else:
+            subprocess.run(["espeak-ng", "--voices"], check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            phoneme_converter = phonemizer.backend.EspeakBackend(language="en-us", preserve_punctuation=True, with_stress=True)
+            print("Using espeak phonemeizer")
+    except FileNotFoundError:
+        if "win" in sys.platform:
+            phoneme_converter = PhonemeConverterFactory.load_phoneme_converter(defined_converter)
+            print("Warning: espeak phoneme converter is not supported on Windows. Using gruut instead.")
+        else:
+            raise RuntimeError("espeak-ng is not installed on your system. Run `sudo apt install espeak-ng`.")
+    return phoneme_converter
